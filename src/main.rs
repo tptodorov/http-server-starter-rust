3c39d6a389e1638fc1a4c0e9fcfeac4adaa31e3e
Todor Todorov <98095+tptodorov@users.noreply.github.com>
2024-04-28 20:35:54 +0200

    response ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
 
@@ -11,8 +12,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                write!(_stream, "HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                _stream.flush().unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
