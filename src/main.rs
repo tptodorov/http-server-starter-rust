01cbe558e6f868b915c588ca09c56b7794a52de1
Todor Todorov <98095+tptodorov@users.noreply.github.com>
2024-04-30 10:26:56 +0200

    header and response structs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::{BufRead, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use itertools::Itertools;
+use anyhow::Result;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -22,33 +25,94 @@ fn main() {
 
                 let mut lines = buffer.lines();
                 let first_line = lines.next().unwrap();
-
+                // read headline
                 let mut parts = first_line.split(' ');
                 let _method = parts.next().filter(|&s| s == "GET" || s == "PUT");
                 let path = parts.next().unwrap().split("/").collect::<Vec<&str>>();
                 let _protocol = parts.next();
                 println!("path: {:?}", path);
-                match path.as_slice() {
+                // read headers
+                let mut headers = HashMap::new();
+                println!("path: {:?}", headers);
+                loop {
+                    let mut header = String::new();
+                    if let Ok(_header_size) = buf_reader.read_line(&mut header) {
+                        println!("header: {:?}", header);
+                        let header = header.trim();
+                        if header ==  "" {
+                            break;
+                        }
+                        if let Some((key,value))=header.split(":").map(|s| s.trim()).collect_tuple() {
+                            headers.insert(key.to_lowercase().to_string(),value.to_string());
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+                let mut response = match path.as_slice() {
                     ["", ""] =>
-                        write!(_stream, "HTTP/1.1 200 OK\r\n\r\n").unwrap(),
+                        Response::new(_stream),
+                    ["", "user-agent"] =>
+                        Response::new(_stream).with_header("Content-Type", "text/plain").with_content(headers.get("user-agent").unwrap_or(&"".to_string())),
                     ["", "echo", rest @ .. ] =>
                         {
                             let content = rest.join("/");
-                            write!(_stream, "HTTP/1.1 200 OK\r\n").unwrap();
-                            write!(_stream, "Content-Type: text/plain\r\n").unwrap();
-                            write!(_stream, "Content-Length: {}\r\n", content.len()).unwrap();
-                            write!(_stream, "\r\n").unwrap();
-                            write!(_stream, "{}", content).unwrap();
-                        }
+                            Response::new(_stream).with_header("Content-Type", "text/plain").with_content(&content)
+                        },
                     _ =>
-                        write!(_stream, "HTTP/1.1 404 Not Found\r\n\r\n").unwrap(),
-                }
-
-                _stream.flush().unwrap()
+                        Response::new(_stream).with_code(404),
+                };
+                println!("response: {:?}",response);
+                response.flush().unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+#[derive(Debug)]
+struct Response {
+    stream:  TcpStream,
+    code: u16,
+    content: String,
+    pub headers: HashMap<String, String>,
+}
+
+impl Response {
+    pub fn  new(stream: TcpStream) -> Self {
+        Response {
+            stream,
+            code: 200,
+            content: "".to_string(),
+            headers: HashMap::new(),
+        }
+    }
+
+    pub fn with_header(mut self, key: &str, value: &str) -> Self {
+        self.headers.insert(key.to_string(), value.to_string());
+        self
+    }
+    pub fn with_code(mut self, code: u16) -> Self {
+        self.code = code;
+        self
+    }
+
+    pub fn with_content(mut self, content: &str) -> Self {
+        self.content = content.to_string();
+        self
+    }
+
+    pub fn flush(&mut self) -> Result<()> {
+        write!(self.stream, "HTTP/1.1 {} OK\r\n", self.code)?;
+        for (k,v) in self.headers.iter() {
+            write!(self.stream, "{}: {}\r\n", k,v)?;
+        }
+        write!(self.stream, "Content-Length: {}\r\n", self.content.len())?;
+        write!(self.stream, "\r\n")?;
+        write!(self.stream, "{}", self.content)?;
+        self.stream.flush()?;
+        Ok(())
     }
 }
